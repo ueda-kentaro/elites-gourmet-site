463a50429f2ee85404a99f18de9d98b2562a71b7
ueda kentaro <nowallcloud9+ueda@gmail.com>
2017-04-23 12:43:02 +0000

    ログイン画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
+
+  devise_for :users
+  root 'restaurants#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
